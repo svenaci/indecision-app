7c02c095b781f1ba4e9c05c8deeeaec6b7670e05
svenaci <steffan_v@hotmail.com>
2023-11-15 17:54:02 -0500

    add styling for option list

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,13 +4,18 @@ import Option from "./Option.js";
 const Options = (props) => {
   return (
     <div>
-      <button
-        className="button button--link"
-        onClick={props.handleDeleteOptions}
-      >
-        Remove all options
-      </button>
-      {props.options.length === 0 && <p>PLease add option to get started!</p>}
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={props.handleDeleteOptions}
+        >
+          Remove all
+        </button>
+      </div>
+      {props.options.length === 0 && (
+        <p className="widget__message">Please add option to get started!</p>
+      )}
       {props.options.map((option, index) => (
         <Option
           key={index}
